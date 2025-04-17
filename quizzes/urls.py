a3d82bbd9bb0a6f4e4d2b704288795e94f3af5fa
sajib1066 <sajib1066@gmail.com>
2025-04-18 02:55:21 +0800

    update url

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -6,7 +6,7 @@ app_name = "quizzes"
 
 
 urlpatterns = [
-    path('quizzes/', views.QuizListView.as_view(), name="quizzes"),
+    path('', views.QuizListView.as_view(), name="quizzes"),
     path("<int:pk>/", views.QuizView.as_view(), name="quiz"),
     path('submit-quiz-answer/', views.submit_quiz_result, name='submit_quiz_result'),
 ]
